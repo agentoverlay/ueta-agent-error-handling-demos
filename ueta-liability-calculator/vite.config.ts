446adfea5aff9b0b941fbe045efbce4a5dae3bed
Andor Kesselman <andor@andor.us>
2025-03-29 20:42:11 -0700

    update

--- ueta-liability-calculator/vite.config.ts
+++ ueta-liability-calculator/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // Get the repository name from package.json or environment variables
 const getRepositoryName = () => {
-  return '/
+  return '/'
 }
 
 // https://vite.dev/config/
